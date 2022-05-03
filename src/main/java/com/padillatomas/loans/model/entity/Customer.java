040c38b4c758b68de54c1692faf0cdea5f2198b5
Tomas Padilla <padillatomasagustin@gmail.com>
2022-05-03 11:17:53 +0200

    Loans - Entities

---
+++ src/main/java/com/padillatomas/loans/model/entity/Customer.java
@@ -0,0 +1,27 @@
+package com.padillatomas.loans.model.entity;
+
+import lombok.Data;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@ToString
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "customer_id")
+    private Long customerId;
+
+    private String name;
+
+    private String email;
+
+    @Column(name = "mobile_number")
+    private String mobileNumber;
+
+    @Column(name = "create_dt")
+    private LocalDate createDt;
+}
