22fe4b32b418065338057ca6d398d3c9f1c03d0a
Tomas Padilla <padillatomasagustin@gmail.com>
2022-05-04 19:38:12 +0200

    Cards - ConfigServer OK

--- src/main/java/com/padillatomas/cards/controller/CardsController.java
+++ src/main/java/com/padillatomas/cards/controller/CardsController.java
@@ -1,9 +1,16 @@
 package com.padillatomas.cards.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.padillatomas.cards.config.CardsServiceConfig;
+import com.padillatomas.cards.model.Properties;
 import com.padillatomas.cards.model.entity.Cards;
 import com.padillatomas.cards.model.entity.Customer;
 import com.padillatomas.cards.repository.CardsRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +23,25 @@ public class CardsController {
 
     private final CardsRepository cardsRepository;
 
+    @Autowired
+    CardsServiceConfig cardsServiceConfig;
+
+    //    === GET Properties ===
+    @GetMapping("/cards/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        System.out.println(cardsServiceConfig);
+        Properties properties = new Properties(
+                cardsServiceConfig.getMsg(),
+                cardsServiceConfig.getBuildVersion(),
+                cardsServiceConfig.getMailDetails(),
+                cardsServiceConfig.getActiveBranches()
+        );
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
+    }
+
+    //    === My Cards ===
     @PostMapping("/myCards")
     public List<Cards> getCardDetails(@RequestBody Customer customer){
         List<Cards> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
