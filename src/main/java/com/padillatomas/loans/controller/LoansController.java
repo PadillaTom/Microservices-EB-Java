c577036c53bb5e266a97224b220a304d08db90b4
Tomas Padilla <padillatomasagustin@gmail.com>
2022-05-04 11:19:19 +0200

    Added Controller endpoint

--- src/main/java/com/padillatomas/loans/controller/LoansController.java
+++ src/main/java/com/padillatomas/loans/controller/LoansController.java
@@ -1,9 +1,16 @@
 package com.padillatomas.loans.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.padillatomas.loans.config.LoansServiceConfig;
+import com.padillatomas.loans.model.Properties;
 import com.padillatomas.loans.model.entity.Customer;
 import com.padillatomas.loans.model.entity.Loans;
 import com.padillatomas.loans.repository.LoansRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +23,26 @@ public class LoansController {
 
     private final LoansRepository loansRepository;
 
+    @Autowired
+    LoansServiceConfig loansServiceConfig;
+
+    //    === GET Properties ===
+    @GetMapping("/accounts/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        System.out.println(loansServiceConfig);
+        Properties properties = new Properties(
+                loansServiceConfig.getMsg(),
+                loansServiceConfig.getBuildVersion(),
+                loansServiceConfig.getMailDetails(),
+                loansServiceConfig.getActiveBranches()
+        );
+
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
+    }
+
+//    === My Loans ===
     @PostMapping("/myLoans")
     public List<Loans> getLoansDetails(@RequestBody Customer customer) {
         List<Loans> loans = loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
